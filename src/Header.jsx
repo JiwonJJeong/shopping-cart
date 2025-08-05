41c293ffdebd1b882f380157f6be798baf5da86a
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-08-05 14:26:10 -0700

    Feat: Add header with route links

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Header(){
+
+
+    return (
+        <header>
+            <h1>The Odin Shop</h1>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/items">Items</Link></li>
+                    <li><Link to="/checkout">Cart</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
