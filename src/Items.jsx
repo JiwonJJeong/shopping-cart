bc08f35aa55cbef29fc33a2aafff9b4d7065a5b2
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-08-05 17:18:49 -0700

    Feat: Fetch shop items from API
    Use promise.all to avoid waterfall async code

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,6 +1,33 @@
-
+import {useEffect, useState} from 'react';
 
 export default function Items(){
+    const [items, setItems] = useState([]);
+    const [error, setError]= useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        try{
+            const fetchOne = async (id) => {
+                const response = await fetch(`https://fakestoreapi.com/products/${id}`, {mode:"cors"});
+                if (response.status >= 400){
+                    throw new Error("server error");
+                }
+                return response;
+            }
+            const fetchAll = async () => {
+                return await Promise.all(Array.from({length:20}, (_,i)=>i+1).map((id)=>fetchOne(id)));
+            }
+            setLoading(true);
+            const items = fetchAll().map((response)=> response.json())
+                .map((obj)=> {obj.id, obj.title, obj.price, obj.image});
+            setItems(items);
+        } catch (error){
+            setError(error);
+            throw new Error(error);
+        } finally {
+            setLoading(false);
+        }
+    }, []);
 
     return (
         <>
