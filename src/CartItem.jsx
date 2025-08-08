5392822b5e10426930c22bf5fbd3e5a26e67d213
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-08-07 18:03:50 -0700

    Fix: Render and handle change of cart item select
    Make sure to apply onChange to the select element,
    not onClick to the option elements.

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,38 +1,49 @@
-export default function CartItem({img, price, boughtCount, setBoughtCount}){
-
-    function handleOptionClick(value){
-        if (value != boughtCount){
-            setBoughtCount(value);
-        }
+export default function CartItem({ img, price, boughtCount, setBoughtCount }) {
+  function handleOptionChange(e) {
+    const value = Number(e.target.value);
+    if (value != boughtCount) {
+      setBoughtCount(value);
     }
+  }
 
-    function handleUpdateCountClick(e){
-        e.preventDefault();
-        const newBuyCount = e.target.previousSibling.value;
-        setBoughtCount(newBuyCount);
-    }
+  function handleUpdateCountClick(e) {
+    e.preventDefault();
+    const newBuyCount = e.target.previousSibling.value;
+    setBoughtCount(newBuyCount);
+  }
 
-    function handleTrashClick(e){
-        e.preventDefault();
-        setBoughtCount(0);
-    }
+  function handleTrashClick(e) {
+    e.preventDefault();
+    setBoughtCount(0);
+  }
 
-    return (
-        <article>
-            <img src={img}></img>
-            <p>${price*boughtCount}</p>
-            {boughtCount <10 && <select name="buy count" value={boughtCount}>
-                {Array.from({ length: 9 }, (_, i) => i + 1).map((i)=>
-                    <option key={i} value={i} onClick={()=>handleOptionClick(i)}></option>
-                )}
-                <option key="10" value="10+" onClick={()=>handleOptionClick(10)}></option>
-            </select>}
-            {boughtCount >= 10 && <form>
-                    <input type="number" defaultValue={boughtCount}></input>
-                    <button onClick={handleUpdateCountClick}>Update</button>
-                </form>}
-            <button onClick={handleTrashClick}>🗑️</button>
-        </article>
-
-    )
+  return (
+    <article>
+      <img src={img} height="100px"></img>
+      <p>${price * boughtCount}</p>
+      {boughtCount < 10 && (
+        <select
+          name="buy count"
+          value={boughtCount}
+          onChange={handleOptionChange}
+        >
+          {Array.from({ length: 9 }, (_, i) => i + 1).map((i) => (
+            <option key={i} value={i}>
+              {i}
+            </option>
+          ))}
+          <option key="10" value="10">
+            10+
+          </option>
+        </select>
+      )}
+      {boughtCount >= 10 && (
+        <form>
+          <input type="number" defaultValue={boughtCount}></input>
+          <button onClick={handleUpdateCountClick}>Update</button>
+        </form>
+      )}
+      <button onClick={handleTrashClick}>🗑️</button>
+    </article>
+  );
 }
