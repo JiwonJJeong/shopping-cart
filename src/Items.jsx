552593c504c3306ff7401b796d7c9631cee13bb2
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-08-24 13:37:26 -0700

    Feat: Style items page

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,22 +1,29 @@
-import ItemCard  from "./ItemCard.jsx"
-import { Link, useOutletContext } from 'react-router-dom';
-import CartAside from "./CartAside.jsx"
+import ItemCard from "./ItemCard.jsx";
+import { Link, useOutletContext } from "react-router-dom";
+import CartAside from "./CartAside.jsx";
+import "./styles/Items.css";
 
-export default function Items(){
-    const {items, boughtCounts, changeBoughtCountOfId} = useOutletContext();
+export default function Items() {
+  const { items, boughtCounts, changeBoughtCountOfId } = useOutletContext();
 
-    return (
-        <>
-            {items.map((item) => <ItemCard
-                key={item.id}
-                id={item.id}
-                img= {item.img}
-                title={item.title} 
-                price={item.price}
-                boughtCount={boughtCounts[item.id] || 0}
-                setBoughtCount={(newCount)=>changeBoughtCountOfId(item.id)(newCount)} // partial application
-            />)}
-            <CartAside></CartAside>
-        </>
-    )
+  return (
+    <>
+      <div className="cards">
+        {items.map((item) => (
+          <ItemCard
+            key={item.id}
+            id={item.id}
+            img={item.img}
+            title={item.title}
+            price={item.price}
+            boughtCount={boughtCounts[item.id] || 0}
+            setBoughtCount={(newCount) =>
+              changeBoughtCountOfId(item.id)(newCount)
+            } // partial application
+          />
+        ))}
+      </div>
+      <CartAside></CartAside>
+    </>
+  );
 }
